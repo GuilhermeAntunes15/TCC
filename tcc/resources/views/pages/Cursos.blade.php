58857a2e97fbb704e089373b87988d1903037dae
GuilhermeAntunes15 <guiancam@gmail.com>
2021-08-12 08:20:10 -0300

    v1.1.1

--- tcc/resources/views/pages/Cursos.blade.php
+++ tcc/resources/views/pages/Cursos.blade.php
@@ -1,52 +1,5 @@
-<!DOCTYPE html>
-<html lang="pt" class="black-mode">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="{{ asset('css/principal.css') }}" />
-    <link rel="stylesheet" href="{{ asset('css/cursos.css') }}" />
-    <link rel="stylesheet" href="{{ asset('css/navbar.css') }}" />
-    <link
-      rel="shortcut icon"
-      href="{{ asset('img/icone_charlie.png') }}"
-      type="image/x-icon"
-    />
-    <script
-      src="https://kit.fontawesome.com/04cbf46b06.js"
-      crossorigin="anonymous"
-    ></script>
-    <script src="https://cdn.jsdelivr.net/npm/js-cookie@rc/dist/js.cookie.min.js"></script>
-    <script src="{{ asset('js/principal.js') }}"></script>
-    <title>Cursos</title>
-  </head>
-  <body>
-    <nav>
-      <a href="index.html" id="logo"
-        ><img
-          src="{{ asset('img/icone_charlie1-2.png') }}"
-          id="img1"
-          title="logo" />
-        <p>Charlie</p>
-        <img src="{{ asset('img/icone_charlie2-2.png') }}" id="img2" title="logo"
-      /></a>
-      <a href="javascript:void(0);" class="icon" onclick="resp_nav()">
-        <i class="fa fa-bars"></i>
-      </a>
-      <section id="navbar" class="navbar">
-        <i
-          class="fas fa-moon change_mode"
-          onclick="change_mode()"
-          alt="Modo escuro"
-          id="change_mode"
-        ></i>
-        <a href="#" id="nav-link">Sobre nós</a>
-        <a href="cursos.html" id="nav-link" style="text-decoration: underline"
-          >Cursos</a
-        >
-        <a href="inscrever.html" class="botao">Inscreva-se</a>
-      </section>
-    </nav>
+@extends('layouts.layoutPrincipal')
+@section('content')
 
     <!----- MAIN ----->
 
@@ -142,6 +95,4 @@
       </div>
     </main>
 
-    <footer><p>Charlie@2021</p></footer>
-  </body>
-</html>
+    @stop
